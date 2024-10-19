0080e81ebc71fd82145dd08eed80b146f34023aa
Denys Rashevskyy <admin.phoneone@gmail.com>
2024-10-19 17:11:42 +0100

    final commit

--- src/main/java/com/app/TimeServlet.java
+++ src/main/java/com/app/TimeServlet.java
@@ -1,7 +1,7 @@
 package com.app;
 
-import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -11,6 +11,8 @@ import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
 
 
 import java.io.IOException;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
 import java.time.OffsetDateTime;
 import java.time.ZoneId;
 import java.time.ZoneOffset;
@@ -20,26 +22,61 @@ import java.time.format.DateTimeFormatter;
 @WebServlet(value = "/time")
 public class TimeServlet extends HttpServlet {
     private TemplateEngine engine;
-
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    public void init() {
+        engine = new TemplateEngine();
+        initTemplateResolver();
+    }
+
+    private void initTemplateResolver() {
+        ClassLoaderTemplateResolver resolver = new ClassLoaderTemplateResolver();
+        resolver.setPrefix("./templates/");
+        resolver.setSuffix(".html");
+        resolver.setTemplateMode("HTML5");
+        resolver.setOrder(engine.getTemplateResolvers().size());
+        resolver.setCacheable(false);
+        engine.addTemplateResolver(resolver);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         resp.setContentType("text/html");
-        String timezoneParam = req.getParameter("timezone");
+        String timezoneParam = getTimezoneParam(req, resp);
+
         String formattedTime = formatTimeWithTimeZone(timezoneParam);
-
-
         String htmlContent = getHtmlContent(formattedTime);
 
         resp.getWriter().write(htmlContent);
-        resp.getWriter().close();
+
     }
 
-    private String getHtmlContent(String formattedTime) {
-        Context context = new Context();
-        context.setVariable("time", formattedTime);
-        return engine.process("timeTemplate", context);
+    private String getTimezoneParam(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        String timezoneParam = req.getParameter("timezone");
+        if (timezoneParam != null) {
+            addLastTimezoneCookie(resp, timezoneParam);
+        } else {
+            timezoneParam = getLastTimezoneFromCookies(req.getCookies());
+        }
+        return timezoneParam;
+    }
+
+    private void addLastTimezoneCookie(HttpServletResponse resp, String timezoneParam) throws IOException {
+        Cookie lastTimezoneCookie = new Cookie("lastTimezone", URLEncoder.encode(timezoneParam, "UTF-8"));
+        resp.addCookie(lastTimezoneCookie);
+    }
+
+    private String getLastTimezoneFromCookies(Cookie[] cookies) throws IOException {
+        if (cookies == null) {
+            return null;
+        }
+        for (Cookie cookie : cookies) {
+            if ("lastTimezone".equals(cookie.getName())) {
+                return URLDecoder.decode(cookie.getValue(), "UTF-8");
+            }
+        }
+        return null;
     }
 
     private String formatTimeWithTimeZone(String timezoneParam) {
@@ -53,16 +90,9 @@ public class TimeServlet extends HttpServlet {
         return currentTime.format(FORMATTER) + " " + timezone;
     }
 
-    @Override
-    public void init() throws ServletException {
-        engine = new TemplateEngine();
-
-        ClassLoaderTemplateResolver resolver = new ClassLoaderTemplateResolver();
-        resolver.setPrefix("./templates/");
-        resolver.setSuffix(".html");
-        resolver.setTemplateMode("HTML5");
-        resolver.setOrder(engine.getTemplateResolvers().size());
-        resolver.setCacheable(false);
-        engine.addTemplateResolver(resolver);
+    private String getHtmlContent(String formattedTime) {
+        Context context = new Context();
+        context.setVariable("time", formattedTime);
+        return engine.process("timeTemplate", context);
     }
 }
--- src/main/java/com/app/TimezoneValidateFilter.java
+++ src/main/java/com/app/TimezoneValidateFilter.java
@@ -9,7 +9,6 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
-import java.net.URLDecoder;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 import java.util.HashSet;
@@ -30,27 +29,12 @@ public class TimezoneValidateFilter extends HttpFilter {
 
     @Override
     protected void doFilter(HttpServletRequest req, HttpServletResponse resp, FilterChain chain) throws IOException, ServletException {
-        //String timezone = req.getParameter("timezone");
-        String timezoneParam = getTimezoneParameter(req, resp);
-//        if (timezone != null) {
-//            Cookie cookie = new Cookie("lastTimezone", timezone);
-//            resp.addCookie(cookie);
-//            timezoneParam = timezone;
-//        } else {
-//            Cookie[] cookies = req.getCookies();
-//            if (cookies != null) {
-//                for (Cookie cookie : cookies) {
-//                    if (cookie.getName().equals("lastTimezone")) {
-//                        timezoneParam = cookie.getValue();
-//                        break;
-//                    }
-//                }
-//            }
-//        }
-//        if (timezoneParam == null) {
-//            chain.doFilter(req, resp);
-//            return;
-//        }
+        String timezoneParam = req.getParameter("timezone");
+
+        if (timezoneParam == null) {
+            chain.doFilter(req, resp);
+            return;
+        }
         if (timezoneParam.length() < 3) {
             sendError(resp, "Invalid or missing timezone parameter");
             return;
@@ -59,6 +43,9 @@ public class TimezoneValidateFilter extends HttpFilter {
         try {
             int offset = Integer.parseInt(timezoneParam.substring(3).trim());
             if (VALID_OFFSETS.contains(offset)) {
+                String encodedTimezone = URLEncoder.encode(timezoneParam, StandardCharsets.UTF_8.toString());
+                Cookie cookie = new Cookie("lastTimezone", encodedTimezone);
+                resp.addCookie(cookie);
                 chain.doFilter(req, resp);
             } else {
                 sendError(resp, "Invalid timezone offset");
@@ -79,25 +66,5 @@ public class TimezoneValidateFilter extends HttpFilter {
     public void destroy() {
         VALID_OFFSETS.clear();
         super.destroy();
-    }
-
-    private String getTimezoneParameter(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-        String timezoneParameter = req.getParameter("timezone");
-        if (timezoneParameter != null) {
-            String encodedTimezone = URLEncoder.encode(timezoneParameter, StandardCharsets.UTF_8.toString());
-            Cookie cookie = new Cookie("lastTimezone", encodedTimezone);
-            resp.addCookie(cookie);
-            return timezoneParameter;
-        } else {
-            Cookie[] cookies = req.getCookies();
-            if (cookies != null) {
-                for (Cookie cookie : cookies) {
-                    if ("lastTimezone".equals(cookie.getName())) {
-                        return URLDecoder.decode(cookie.getValue(), StandardCharsets.UTF_8.toString());
-                    }
-                }
-            }
-        }
-        return null;
     }
 }
